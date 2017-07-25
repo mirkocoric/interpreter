dce98564fd0833e9235285b4d7d0cada7b3818f5
Mirko Coric <mirkocoric@gmail.com>
2017-07-25 20:00:50 +0200

    rade expressioni

--- logo_commands.py
+++ logo_commands.py
@@ -3,6 +3,8 @@ import util
 from workspace import WS
 from inspect import getargspec
 
+# RIJESITI PR INT 4  PR OR ...
+
 
 class LogoCommands(object):
     def exec_proc(self, command):
@@ -15,46 +17,50 @@ class LogoCommands(object):
             self.execute(line, proc)
         return True
 
-    def execute(self, proc, line):  # MORA BITI U KLASI KAO I NAREDBE
-        # razmisliti moguce je da vrijednost ovisi o buducoj naredbi...t
-        # treba biti rekurzija
-        # SMISLITI REKURZZIJU ALI NAJPRIJE TESTIRATI DA RADI ZA JEDNOSTAVNE NAREDBE
+    def execute(self, proc, line):
         commands = util.split_line(line)
         for command in commands:
             func = getattr(self, command.name, None)
+            expr = [util.calc_expr(arg) for arg in command.args]
             if func:
-                if util.check_num_args(getargspec(func), command.args):
-                    func(proc, *command.args)
+                if util.check_num_args(getargspec(func), expr):
+                    func(proc, *expr)
             else:
                 if not self.exec_proc(command):
-                    print("Command %s does not exist" % command.name)
+                    print("COMMAND %s DOES NOT EXIST" % command.name)
 
-    def load(self, proc, f_name):
-        # try:
-        f = open(f_name, 'r')
-        util.search_file(f)
-        # except ValueError:
-        #    print ("File not found")
-        # finally:
-        #    f.close()
+    def LOAD(self, proc, f_name):
+        if not os.path.exists(f_name):
+            print("FILE NOT FOUND")
+            return
+        with open(f_name, 'r') as f:
+            util.search_file(f)
 
-    def save(self, proc, f_name):
+    def SAVE(self, proc, f_name):
         if os.path.exists(f_name):
-            print("File already exists")
+            print("FILE ALREADY EXISTS")
             return
         with open(f_name, 'w') as f:
             util.save_in_file(f)
 
-    def local(self, proc, var):
+    def LOCAL(self, proc, var):
         if proc:
             proc.vars.update({var, None})
         else:
-            print "Can only do that in a procedure"
+            print "CAN ONLY DO THAT IN A PROCEDURE"
 
-    def make(self, proc, name, value):
-        value = util.calc_expr(value)
-        WS.vars.update({name: value})
+    def MAKE(self, proc, name, value):
+        if value:
+            WS.vars.update({name: value})
 
-    def pons(self, proc):
+    def PONS(self, proc):
         for var in WS.vars:
             print "%s IS %s" % (var, WS.vars[var])
+
+    def INT(self, proc, value):
+        if util.is_float(value):
+            return int(value)
+        print "INT DOESN'T LIKE %s AS INPUT"
+
+    def PR(self, proc, value):
+        print value
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import re
 from workspace import WS
 from inspect import getargspec
 
@@ -7,8 +8,147 @@ Command = namedtuple('Command', 'name args')
 Procedure = namedtuple('Procedure', 'body args vars')
 
 
+def is_int(name):
+    """Returns True if the string is int"""
+    try:
+        float_name = float(name)
+        int_name = int(float_name)
+        if int_name == float_name:
+            return True
+        return False
+    except ValueError:
+        return False
+
+
+def is_float(name):
+    """Returns True if it is possible to convert string to float"""
+    try:
+        float(name)
+        return True
+    except ValueError:
+        return False
+
+
 def calc_expr(arg):
-    return arg
+    expr = arg_to_list(arg)
+    operators = []
+    operands = []
+    for item in expr:
+        if item == ")":
+            while True:
+                try:
+                    operator = operators.pop()
+                except:
+                    print_parse_error(")")
+                    return None
+                if operator == "(":
+                    break
+                operands.append(perform_operation(operands, operator))
+        elif (is_plus_minus(item) and
+              is_em_prod_div(operators)):
+            while operators:
+                operands.append(perform_operation(operands, operators.pop()))
+            operators.append(item)
+        elif is_operator(item):
+            operators.append(item)
+        else:
+            result = parse(item)
+            if result:
+                operands.append(result)
+            elif WS.proc.get(item):
+                operators.append(WS.proc.get(item))
+            else:
+                if not item.startswith(":"):
+                    print_parse_error(item)
+                return None
+
+    while operators:
+        operands.append(perform_operation(operands, operators.pop()))
+    return operands.pop()
+
+
+def print_parse_error(item):
+    print ("I DON'T KNOW WHAT TO DO WITH %s"
+           % item)
+
+
+def print_undefined(item):
+    print "I DON'T KNOW HOW TO %s" % item
+
+
+def parse(item):
+    if item.startswith("\""):
+        return item[1:]
+    elif is_int(item):
+        return int(item)
+    elif is_float(item):
+        return float(item)
+    elif item.startswith(":"):
+        if WS.vars.get(item[1:]):
+            return WS.vars.get(item[1:])
+        else:
+            print "%s HAS NO VALUE" % item[1:]
+    return None
+
+
+def perform_operation(operands, operator):
+    operand2 = operands.pop()
+    try:
+        operand1 = operands.pop()
+    except IndexError:
+        return operand2
+    if operator == "+":
+        return operand1 + operand2
+    if operator == "-":
+        return operand1 - operand2
+    if operator == "*":
+        return operand1 * operand2
+    if operator == "/":
+        return float(operand1) / operand2
+    if operator == "=":
+        return operand1 == operand2
+
+
+def is_operator(ch):
+    operators = "()-+*/="
+    for operator in operators:
+        if ch == operator:
+            return True
+    return False
+
+
+def is_plus_minus(ch):
+    operators = "-+"
+    for operator in operators:
+        if ch == operator:
+            return True
+    return False
+
+
+def is_em_prod_div(operators):
+    if not operators:
+        return True
+    prod_div = "*/"
+    for operator in prod_div:
+        if operators[- 1:][0] == operator:
+            return True
+    return False
+
+
+def arg_to_list(arg):
+    operand = ""
+    l = []
+    for ch in arg:
+        if is_operator(ch):
+            if operand:
+                l.append(operand)
+                operand = ""
+            l.append(ch)
+        else:
+            operand += ch
+    if operand:
+        l.append(operand)
+    return l
 
 
 def check_num_args(func_args, args):
@@ -19,8 +159,7 @@ def check_num_args(func_args, args):
     if func_args.varargs:
         n_args += len(func_args.varargs)
     if len(args) > n_args:
-        print ("I don't know what to do with %s"
-               % args[n_args])
+        print_parse_error(args[n_args])
         return False
     return True
 
